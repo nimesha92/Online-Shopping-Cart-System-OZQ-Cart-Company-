229637b86eadeb3cb6f03b170c08665146631494
unknown <nimeshakalaz92916@gmail.com>
2023-12-01 16:38:32 +0530

    intial Commit

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import './Footer.css'
+
+const Footer = () => {
+    return ( 
+        <footer>
+            <div className="footer__container">
+
+                <div className="fotter__copyright__container">
+                    <ul className='nav'>
+                        <li className="footer__copyright">©2023 OZQ |</li>
+
+                    </ul>
+                </div>
+            </div>
+        </footer>
+     );
+}
+ 
+export default Footer;
